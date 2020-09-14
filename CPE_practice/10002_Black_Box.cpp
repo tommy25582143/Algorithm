15249c3ee715a4fa7acb5164c9f25be4e808f414
Darklilnightc <tommy25582143@gmail.com>
2020-09-14 17:21:54 +0800

    2020/9/14

---
+++ CPE_practice/10002_Black_Box.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>	
+#include <queue>
+#include <algorithm>
+#include <string>
+
+
+using namespace std;
+
+
+int main() {
+
+	int m, n;
+	cin >> m >> n;
+
+	vector<int>elements;
+	elements.reserve(m);
+	vector<int>get;
+	get.reserve(n);
+	vector<int>black_box;
+	black_box.reserve(n);
+
+	
+	int temp;
+	while (m-- > 0) {
+		
+		cin >> temp;
+		elements.push_back(temp);
+
+	}
+	while (n-- > 0) {
+		
+		cin >> temp;
+		get.push_back(temp);
+
+	}
+
+	vector<int>::iterator next_get =  get.begin();
+	int i = 0;
+	bool first_in = true;
+	int counter = 0;
+	while (1) {
+
+
+		if (*next_get == black_box.size()) {
+			if (first_in) {
+				first_in = false;
+			}
+			else {
+				cout << endl;
+			}
+			cout << black_box[i];
+			next_get++;
+			i++;
+		}
+
+		if (next_get == get.end()) {
+			break;
+		}
+
+		
+		if (*next_get != counter && counter < elements.size()) {
+			black_box.push_back(elements[counter]);
+			sort(black_box.begin(), black_box.end());
+			counter++;
+		}
+
+	}
+	return 0;
+}
---
+++ CPE_practice/10006_Moscow_Time.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <vector>	
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <cstring>
+
+
+using namespace std;
+
+
+int main() {
+
+	char day[3];
+	int date;
+	char month[3];
+	int year;
+	int hour, minute, second;
+	char time_zone[3];
+
+	scanf("%3s, %2d %3s %d %d:%d:%d %s", day, date, month, year, hour, minute, second, time_zone);
+
+	int day_number;
+	if (strcmp(day, "MON") == 0) {
+		day_number = 1;
+	}
+	else if (strcmp(day, "TUE") == 0) {
+		day_number = 2;
+	}
+	else if (strcmp(day, "WED") == 0) {
+		day_number = 3;
+	}
+	else if(strcmp(day, "THU") == 0){
+		day_number = 4;
+	}
+	else if (strcmp(day, "FRI") == 0) {
+		day_number = 5;
+	}
+	else if (strcmp(day, "SAT") == 0) {
+		day_number = 6;
+	}
+	else {
+		day_number = 7;
+	}
+
+	int month_number;
+	if (strcmp(month, "JAN") == 0) {
+		month_number = 1;
+	}
+	else if (strcmp(month, "FEB") == 0) {
+		month_number = 2;
+	}
+	else if (strcmp(month, "MAR") == 0) {
+		month_number = 3;
+	}
+	else if (strcmp(month, "APR") == 0) {
+		month_number = 4;
+	}
+	else if (strcmp(month, "MAY") == 0) {
+		month_number = 5;
+	}
+	else if (strcmp(month, "JUN") == 0) {
+		month_number = 6;
+	}
+	else if (strcmp(month, "JUL") == 0) {
+		month_number = 7;
+	}
+	else if (strcmp(month, "AUG") == 0) {
+		month_number = 8;
+	}
+	else if (strcmp(month, "SEP") == 0) {
+		month_number = 9;
+	}
+	else if (strcmp(month, "OCT") == 0) {
+		month_number = 10;
+	}
+	else if (strcmp(month, "NOV") == 0) {
+		month_number = 11;
+	}
+	else {
+		month_number = 12;
+	}
+	return 0;
+}
