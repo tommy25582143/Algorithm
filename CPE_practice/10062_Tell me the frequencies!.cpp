be96603cdcac360a36fd142640a01a8b3008543b
Unknown <tommy25582143@gmail.com>
2021-01-26 23:14:24 +0800

    2021/1/26

---
+++ CPE_practice/10062_Tell me the frequencies!.cpp
@@ -0,0 +1,60 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
+
+bool compare(const pair<int, int>&a, const pair<int, int>&b) {
+
+	if (a.second != b.second) {
+		return a.second < b.second;
+	}
+	else {
+		return a.first > b.first;
+	}
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	string temp;
+	bool first_in = true;
+	while (getline(cin, temp)) {
+		if (!first_in) {
+			cout << '\n';
+		}
+		else {
+			first_in = false;
+		}
+		vector<pair<int, int>>a(95, make_pair(0, 10000));
+		int i = 0;
+		for (auto itr = a.begin(); itr != a.end(); itr++, i++) {
+			itr->first = i;
+		}
+
+		for (int i = 0; i < temp.size(); i++) {
+	
+			if (a[temp[i] - 32].second == 10000) {
+				a[temp[i] - 32].second = 1;
+			}
+			else {
+				a[temp[i] - 32].second++;
+			}
+			
+		}
+		sort(a.begin(), a.end(), compare);
+		auto itr = a.begin();
+		while (itr->second != 10000) {
+			cout << itr->first + 32 << " " << itr->second << '\n';
+			itr++;
+		}
+	}
+	return 0;
+}
---
+++ CPE_practice/11639_Guard the Land.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+
+
+	//FILE* output;
+	//output = fopen("output.txt", "w");
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int input;
+	cin >> input;
+	int night = 1;
+	while (night <= input) {
+		int x1, x2, y1, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		int x3, x4, y3, y4;
+		cin >> x3 >> y3 >> x4 >> y4;
+		
+		int i = max(x1, x3);
+		int k = min(x2, x4);
+		int j = max(y1, y3);
+		int l = min(y2, y4);
+
+		int highly_secured = -1;
+		int secured = -1;
+		int unsecured = -1;
+
+		if (i >= k || j >= l) {
+			highly_secured = 0;
+		}
+		else {
+			highly_secured = (i - k) * (j - l);
+		}
+		secured = (x2 - x1) * (y2 - y1) + (x4 - x3) * (y4 - y3) - 2 * highly_secured;
+		unsecured = 100 * 100 - highly_secured - secured;
+		printf("Night %d: %d %d %d\n", night, highly_secured, secured, unsecured);
+		//fprintf(output, "Night %d: %d %d %d\n", i, highly_secured, secured, unsecured);
+		night++;
+	}	
+	//fclose(output);
+	return 0;
+}
