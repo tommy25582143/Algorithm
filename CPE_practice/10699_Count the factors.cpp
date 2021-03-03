8ebad44b85d83f69fe615628635c40728468adc9
Unknown <tommy25582143@gmail.com>
2021-03-03 20:13:06 +0800

    2021/3/3

---
+++ CPE_practice/10699_Count the factors.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+
+int main() {
+
+	//ofstream cout;
+	//cout.open("cout.txt");
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	vector<int>prime;
+	prime.push_back(2);
+	for (int i = 3; i * i <= 1000000; i++) {
+		bool is_prime = true;
+		for (int j = 0; j < prime.size(); j++) {
+			if (i % prime[j] == 0) {
+				is_prime = false;
+				break;
+			}
+		}
+		if (is_prime) {
+			prime.push_back(i);
+		}
+	}
+
+	int input;
+	while ((cin >> input, input) != 0) {
+		cout << input << " : ";
+		int n = 0;
+		for (int i = 0; i < prime.size() && prime[i] <= input; i++) {
+			if (input % prime[i] == 0) {
+				n++;
+				while (input % prime[i] == 0) {
+					input /= prime[i];
+				}
+			}
+		}
+		if (input != 1) {
+			n++;
+		}
+		cout << n << '\n';
+	}
+	//cout.close();
+	return 0;
+}
---
+++ CPE_practice/11332_Summing Digits.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+long sum_digit(string n) {
+	if (n.size() == 1) {
+		return n[0] - '0';
+	}
+	else {
+		long sum = 0;
+		for (int i = 0; i < n.size(); i++) {
+			sum += n[i] - '0';
+		}
+		return sum_digit(to_string(sum));
+	}
+}
+int main() {
+
+	//ofstream cout;
+	//cout.open("cout.txt");
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	string n;
+	while ((cin >> n, n) != "0") {
+		cout << sum_digit(n) << '\n';
+	}
+
+
+	//cout.close();
+	return 0;
+}
